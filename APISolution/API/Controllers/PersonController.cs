2f1b59907b826f0666abd580d87885e779da8c9a
raffaele <raff.iuliani@gmail.com>
2022-09-05 21:29:45 -0400

    Changed controller name

--- APISolution/API/Controllers/PersonController.cs
+++ APISolution/API/Controllers/PersonController.cs
@@ -5,7 +5,7 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DatabaseController : ControllerBase
+    public class PersonController : ControllerBase
     {
     }
 }
