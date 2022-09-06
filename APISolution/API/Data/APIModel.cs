057ef7f87fbd4d206bcdc5ae66a7272e45b5d776
raffaele <raff.iuliani@gmail.com>
2022-09-05 21:27:18 -0400

    Added data model.

---
+++ APISolution/API/Data/APIModel.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Data
+{
+    public class APIModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+
+    }
+}
